96a4ab344cf011206ad2bb76fa1c743bd4ac5a5b
dimabreus <dmitriybreus5@gmail.com>
2024-01-18 15:50:03 +0300

    SVG Circle

--- main.js
+++ main.js
@@ -24,9 +24,10 @@ console.log(`
 
 
  `);
- //Рандомное число на всю игру
- let Randomall = Math.ceil(Math.random() * 8);
- console.log(Randomall)
+//Рандомное число на всю игру
+const Randomall = Math.ceil(Math.random() * 8);
+
+console.log(Randomall)
 //Рандомное число на 1 раунд для игрока
 
 let Randomin1;
@@ -34,14 +35,16 @@ let Randomin1;
 const handleClick = () => {
     Randomin1 = Math.ceil(Math.random() * 8);
     console.log(Randomin1)
+
     //console.log(Math.ceil(Math.random() * 7 + 1))
-    //проверка
-    if (Randomall == Randomin1){
+
+    // Проверка
+
+    if (Randomall === Randomin1) {
         console.log("опа, ты сдох");
-    }
-    else{
+    } else {
         console.log("ФУХ! ты еще жив");
     }
- }
+}
 
- 
+
